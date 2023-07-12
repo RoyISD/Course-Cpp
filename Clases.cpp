7364cd3b1e5e3c62e758c35b9b912025dc44aa10
Rodrigo Reyes <roy.reyes3099@outlook.com>
2023-07-11 21:17:24 -0600

    Funciones y clases amigas

--- Clases.cpp
+++ Clases.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 //Nota: Por defecto se tiene la visibilidad private
 // Buenas practicas: Solo dos secciones 
+class Supervisor;
 class Empleado{
     //visibilidad 
     public: //Permite establecer visibles los miembros desde afuera
@@ -15,11 +16,12 @@ class Empleado{
         float calcularSueldoMensual(int numeroPagos);  // Es hacer la misma funcion pero con parametros
         string nombreCompleto();
         friend class Supervisor; // Haciendo que la clase Supervisor sea amiga de la clase empleado
+        friend float calcualarSueldoMedioAnualPorDepartamento(Supervisor* supervisor, Empleado* empleado1, Empleado* empleado2);
 
         // Métodos Observadors (getters) - obtener la información
         const string* getNombre() const; // Prohibo alterar el estado del objeto const after, el valor
         const string* getApellido() const;
-        const float* getSueldoAnual() const;
+        float getSueldoAnual() const;
         const int getNumeroPagos() const;
 
         //Métodos modificadores (Setters)
@@ -35,11 +37,21 @@ class Empleado{
 // Demostración clases amigas
 class Supervisor{
     public:
+
+        void setNombre(string nombre);
+        void setApellido(string apellido);
+        void setSueldoAnual(float sueldoAnual);
         void mostrarSueldoAnualEmpleado(const Empleado* empleado);
+        friend float calcualarSueldoMedioAnualPorDepartamento(Supervisor* supervisor, Empleado* empleado1, Empleado* empleado2);
+
     private: 
+
+        string nombre, apellido;
+        float sueldoAnual;
+        static const int numeroPagos = 12;
 };
 
-//Declaración de métodos
+//Declaración de métodos Empleado
 float Empleado::calcularSueldoMensual(){
     return (sueldoAnual/numeroPagos);
 }
@@ -57,11 +69,20 @@ void Empleado::setSueldoAnual(float sueldoAnual){this->sueldoAnual = sueldoAnual
 // Getters - devuelven una copia del valor que se desea obtener
 const string* Empleado::getNombre() const {return &nombre;} // Importante el const hace que usuario no pueda modificar el valor del objeto
 const string* Empleado:: getApellido() const{return &apellido;}
-const float* Empleado::getSueldoAnual() const{return &sueldoAnual;}
+float Empleado::getSueldoAnual() const{return sueldoAnual;}
 const int Empleado::getNumeroPagos() const{return numeroPagos;}
+
+
+// Declaración Métodos Supervisor
+void Supervisor::setNombre(string nombre){this->nombre = nombre;}
+void Supervisor::setApellido(string apellido){this->apellido = apellido;}
+void Supervisor::setSueldoAnual(float sueldoAnual){this->sueldoAnual = sueldoAnual;}
 
 void Supervisor::mostrarSueldoAnualEmpleado(const Empleado* empleado){
     cout << "El sueldo anual del empleado es: " << empleado->sueldoAnual << endl;
+}
+float calcualarSueldoMedioAnualPorDepartamento(Supervisor* supervisor, Empleado* empleado1, Empleado* empleado2){
+    return (supervisor->sueldoAnual + empleado1->sueldoAnual + empleado2->sueldoAnual)/3;
 }
 
 int main(){
@@ -75,11 +96,16 @@ int main(){
     //float sueldoEmpleado2 = p->calcularSueldoMensual();
 
     // Función amiga
-    Empleado* p_empleado = new Empleado();
-    p_empleado->setSueldoAnual(20200);
-
+    Empleado* p_empleado1 = new Empleado();
+    Empleado* p_empleado2 = new Empleado();
     Supervisor* p_supervisor = new Supervisor();
-    p_supervisor->mostrarSueldoAnualEmpleado(p_empleado);
+    p_empleado1->setSueldoAnual(20200);
+    p_empleado2->setSueldoAnual(30200);
+    p_supervisor->setSueldoAnual(50200);
 
+    //Supervisor* p_supervisor = new Supervisor();
+    p_supervisor->mostrarSueldoAnualEmpleado(p_empleado1);
+
+    cout << "Sueldo medio dep.: " << calcualarSueldoMedioAnualPorDepartamento(p_supervisor,p_empleado1,p_empleado2) << endl;;
     return 0;
 }
