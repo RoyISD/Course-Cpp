12dfa22d3e739bdc7e2219c9efd1fa5f79a1638d
Rodrigo Reyes <roy.reyes3099@outlook.com>
2023-07-11 20:05:13 -0600

    Clase amiga y codigo funcionando

--- Clases.cpp
+++ Clases.cpp
@@ -11,14 +11,15 @@ using namespace std;
 class Empleado{
     //visibilidad 
     public: //Permite establecer visibles los miembros desde afuera
-        float calcularSueldoMensual();  // Sobrecarga de operadores
+        float calcularSueldoMensual();  // Sobrecarga de métodos
         float calcularSueldoMensual(int numeroPagos);  // Es hacer la misma funcion pero con parametros
         string nombreCompleto();
+        friend class Supervisor; // Haciendo que la clase Supervisor sea amiga de la clase empleado
 
         // Métodos Observadors (getters) - obtener la información
         const string* getNombre() const; // Prohibo alterar el estado del objeto const after, el valor
-        string getApellido() const;
-        float getSueldoAnual() const;
+        const string* getApellido() const;
+        const float* getSueldoAnual() const;
         const int getNumeroPagos() const;
 
         //Métodos modificadores (Setters)
@@ -31,40 +32,54 @@ class Empleado{
         float sueldoAnual;
         static const int numeroPagos = 12; //Static = se trata de atributo compartido por todos los objetos const = no podra ser modificado ??
 };
+// Demostración clases amigas
+class Supervisor{
+    public:
+        void mostrarSueldoAnualEmpleado(const Empleado* empleado);
+    private: 
+};
 
 //Declaración de métodos
 float Empleado::calcularSueldoMensual(){
     return (sueldoAnual/numeroPagos);
 }
 float Empleado::calcularSueldoMensual(int numeroPagos){
-    return (sueldoAnual/numeroPagos);
+    return (sueldoAnual/numeroPagos); // Para referise al atributo de la clase es con this->numeroPagos
 }
 string Empleado::nombreCompleto(){
     return (nombre + " " + apellido);
 }
 // Setters - inicializan el valor de la varible
-void Empleado::setNombre(string nombre){nombre = nombre;}
-void Empleado::setApellido(string apellido){apellido = "Román";}
-void Empleado::setSueldoAnual(float sueldoAnual){sueldoAnual = 28750.45;} 
+void Empleado::setNombre(string nombre){this->nombre = nombre;}
+void Empleado::setApellido(string apellido){this->apellido = apellido;}
+void Empleado::setSueldoAnual(float sueldoAnual){this->sueldoAnual = sueldoAnual;} 
 
 // Getters - devuelven una copia del valor que se desea obtener
 const string* Empleado::getNombre() const {return &nombre;} // Importante el const hace que usuario no pueda modificar el valor del objeto
-string Empleado:: getApellido() const{return apellido;}
-float Empleado::getSueldoAnual() const{return sueldoAnual;}
+const string* Empleado:: getApellido() const{return &apellido;}
+const float* Empleado::getSueldoAnual() const{return &sueldoAnual;}
 const int Empleado::getNumeroPagos() const{return numeroPagos;}
+
+void Supervisor::mostrarSueldoAnualEmpleado(const Empleado* empleado){
+    cout << "El sueldo anual del empleado es: " << empleado->sueldoAnual << endl;
+}
 
 int main(){
 
     //Manejo automatico de memoria //Objeto temporal dentro del main
-    Empleado empleado1;
-    float sueldoEmpleado1 = empleado1.calcularSueldoMensual();
+   // Empleado empleado1;
+    //float sueldoEmpleado1 = empleado1.calcularSueldoMensual();
 
     //Manejo NO automatico
-    Empleado *p = new Empleado(); //Solicitamos memoria acudimos al puntero
-    float sueldoEmpleado2 = p->calcularSueldoMensual();
+    //Empleado *p = new Empleado(); //Solicitamos memoria acudimos al puntero
+    //float sueldoEmpleado2 = p->calcularSueldoMensual();
 
-    
-    printf("Nombre: %s", empleado1.getNombre());
+    // Función amiga
+    Empleado* p_empleado = new Empleado();
+    p_empleado->setSueldoAnual(20200);
+
+    Supervisor* p_supervisor = new Supervisor();
+    p_supervisor->mostrarSueldoAnualEmpleado(p_empleado);
 
     return 0;
 }
