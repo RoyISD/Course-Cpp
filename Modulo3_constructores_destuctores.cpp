cfc969d660bc2fd6cfab56cb92ca142c56288f63
Rodrigo Reyes <roy.reyes3099@outlook.com>
2023-08-22 16:13:25 -0600

    Se agrega ejemplo de constructor copia

--- Modulo3_constructores_destuctores.cpp
+++ Modulo3_constructores_destuctores.cpp
@@ -6,6 +6,9 @@ using namespace std;
 class Empleado{
     public: 
         Empleado(); // Constructor
+        // LISTAS DE INICIALIZACION ??? 
+        //Empleado(string n, string pa) : Nombre(n), PrimerApellido(pa) {sueldoAnual = 60000;}; // Constructor parametrizado
+        Empleado(const Empleado& empleadoOriginal);
         Empleado(string nombre, string apellido);
         float calcularSueldoMensual();
         string NombreCompleto();
@@ -29,6 +32,15 @@ int main(){
 
     Empleado* empleado2 = new Empleado("Brisa", "Reyes");
     cout << empleado2->NombreCompleto() << endl;
+
+    Empleado empleado = Empleado();
+    empleado.setNombre("Evan");
+    empleado.setPrimerApellido("Craft");
+
+    // Usando constructor copia 
+    Empleado* p_copia = new Empleado(empleado);
+    cout << p_copia->NombreCompleto() << endl;
+
     return 0;
 }
 
@@ -41,9 +53,16 @@ string Empleado::NombreCompleto(){
 void Empleado::setNombre(string _nombre_){this->Nombre = _nombre_;}
 void Empleado::setPrimerApellido(string _apellido_){this->PrimerApellido = _apellido_;}
 void Empleado::setSueldoAnual(float _sueldo_){this->sueldoAnual = _sueldo_;}
+// Constructor 
 Empleado::Empleado(){
     Nombre = "Rodrigo";
     PrimerApellido = "Reyes";
+}
+// Constructor copia - copias de objetos
+Empleado::Empleado(const Empleado& empleadoOriginal){
+    Nombre = empleadoOriginal.Nombre;
+    PrimerApellido = empleadoOriginal.PrimerApellido;
+    sueldoAnual = empleadoOriginal.sueldoAnual;
 }
 Empleado::Empleado(string nombre, string apellido){
     this->Nombre = nombre;
